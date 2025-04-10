04796e807b7aa0e4c7af623138f42fb2e72b197d
kansowi <kanumalasolomon@gmail.com>
2025-04-10 12:00:00 +0530

    fixed paddle bug, now responsive

--- game/paddle.py
+++ game/paddle.py
@@ -1,24 +1,24 @@
 import pygame
 from config import WHITE, SCREEN_HEIGHT, PADDLE_SPEED, PADDLE_HEIGHT, PADDLE_WIDTH
-class Paddle(pygame.sprite.Sprite):
-    WIDTH = PADDLE_WIDTH
-    HEIGHT = PADDLE_HEIGHT
+
+class Paddle(pygame.sprite.Sprite):    
+    WIDTH = PADDLE_WIDTH  
+    HEIGHT = PADDLE_HEIGHT 
 
     def __init__(self, x, initial_y_center, color=WHITE):
-        super().__init__()
+        super().__init__() 
 
-        self.image = pygame.Surface([PADDLE_WIDTH, PADDLE_HEIGHT])
-        self.image.fill(color)
+        self.image = pygame.Surface([self.WIDTH, self.HEIGHT])
+        self.image.fill(color)        
         self.rect = self.image.get_rect()
-
         self.rect.x = x
-        self.rect.centery = initial_y_center
+        self.rect.centery = initial_y_center        
         self.speed = PADDLE_SPEED
 
     def move(self, direction):
-        if direction == 1:  # down
+        if direction == 1: 
             self.rect.y += self.speed
-        elif direction == -1:  # up
+        elif direction == -1:
             self.rect.y -= self.speed
         if self.rect.top < 0:
             self.rect.top = 0
