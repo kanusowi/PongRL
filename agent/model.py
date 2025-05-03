67286dd51ea33925071dbb8055b5fcb9cd7d17e9
kansowi <kanumalasolomon@gmail.com>
2025-05-03 12:00:00 +0530

    QNet NN and custom weights

--- agent/model.py
+++ agent/model.py
@@ -5,14 +5,13 @@ from config import FC1_UNITS, FC2_UNITS
 
 class QNetwork(nn.Module):
     def __init__(self, state_size, action_size, seed, fc1_units=FC1_UNITS, fc2_units=FC2_UNITS):
-
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-
-        # nn
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        
+        self._initialize_weights()
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -23,5 +22,5 @@ class QNetwork(nn.Module):
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))  
-        return self.fc3(x) # linear out
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
